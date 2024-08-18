52b4d7f8633a777321f6a9d65f15cb2264ea9637
Pravdeep Kullar <pravdeepkk@gmail.com>
2024-08-18 17:09:17 -0400

    fixing issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import SearchBar from './SearchBar';
 import Header from './Header';
 import Footer from './Footer';
 import LyricsComponent from './APILyrics';
